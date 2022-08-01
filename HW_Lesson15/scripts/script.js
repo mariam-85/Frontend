0806971ed14814863c94526b116bd8b4ee3ccea2
mariam-85 <mariam.lofink85@gmail.com>
2022-08-01 17:35:13 +0200

    homework to lesson15 added

---
+++ HW_Lesson15/scripts/script.js
@@ -0,0 +1,27 @@
+let a = 100;
+console.log(a / 4); //Task1
+
+let b = 8;
+let c = 5;
+let d = 3;
+
+console.log(b / 2 + c - d / 2); //Task2
+
+let string1 = 'Lorem ipsum';
+let string2 = 'dolor sit amet';
+
+console.log(string1 + ' ' + string2); //Task3
+
+console.log(`${string1} ${string2}`); //Task4
+
+let number = 8;
+number = 15;
+console.log(number); //Task5
+
+let sum = 8 ** 2;
+console.log(sum); //Task6
+
+let firstname = 'Mariam';
+let lastname = 'Lofink';
+console.log(`${firstname} ${lastname}`); //Task7
+
