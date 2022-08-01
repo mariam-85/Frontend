87b6be3a3188b8357ebf39142460f27854f2293e
mariam-85 <mariam.lofink85@gmail.com>
2022-08-01 16:59:46 +0200

    new lesson added

---
+++ Lesson15ver2/scripts/script.js
@@ -0,0 +1,58 @@
+// number_of_workers - snake_case
+// numberOfWorkers - camelCase
+// number-of-workers - kebab-case
+// numberofworkers - очень плохой способ
+
+// let number = 5; //chislo
+// let greeting = 'Hello'; //stroka
+// let firstname = 'Oleg'; //stroka
+// let city = `Berlin`;
+
+
+// '', "" - rasnicy net
+// `` - mozhno vkluchat peremennye
+
+/* let a = 5;
+let b = 7;
+console.log(a + b); */
+
+// console.log('Greeting cheres konkatinaciu: ' + greeting + ' ' + firstname); //konkatenacia
+
+// console.log(`Greeting cheres interpolyaciu: ${greeting} ${firstname}`); //interpolyacia
+
+/* let a = 5;
+let b = 'Hello';
+
+let obj = true;
+    
+console.log(typeof a); //number
+console.log(typeof (b)); //string
+console.log(typeof obj); */
+
+let a = (7 + '7');
+let b = (7 - '7h');
+
+let c = +'100h';
+console.log(c);
+console.log(typeof c);
+
+
+/* console.log(a);
+console.log(b);
+console.log(c / 2); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
