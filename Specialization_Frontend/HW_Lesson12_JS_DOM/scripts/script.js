7addc58d8c3888ed60b72db6a824c55f76f2bfce
mariam-85 <mariam.lofink85@gmail.com>
2022-09-14 22:30:06 +0200

    homework added

--- Specialization_Frontend/HW_Lesson12_JS_DOM/scripts/script.js
+++ Specialization_Frontend/HW_Lesson12_JS_DOM/scripts/script.js
@@ -4,50 +4,72 @@
 //Применить стили к карточке: width, background-color, border, border-radius, padding, margin.//
 
 
-const root = document.querySelector('.root');
+// const root = document.querySelector('.root');
 
-root.style.padding = '20px';
-root.style.margin = '10px';
-root.style.backgroundColor = 'Gray';
-root.style.border = 'black';
-root.style.borderRadius = '5px';
-
-
+// root.style.padding = '20px';
+// root.style.margin = '10px';
+// root.style.backgroundColor = 'Gray';
+// root.style.border = 'black';
+// root.style.borderRadius = '5px';
 
 
-const elem1 = document.querySelector('.root .code');
-
-elem1.innerText = 'Vendor code: 123';
-
-elem1.style.padding = '10px';
 
 
-const elem2 = document.querySelector('.root .name');
+// const elem1 = document.querySelector('.root .code');
 
-elem2.innerText = 'Product name: Dress';
+// elem1.innerText = 'Vendor code: 123';
 
-elem2.style.padding = '10px';
+// elem1.style.padding = '10px';
 
 
-const elem3 = document.querySelector('.root .price');
+// const elem2 = document.querySelector('.root .name');
 
-elem3.innerText = 'Price: 140€';
+// elem2.innerText = 'Product name: Dress';
 
-elem3.style.padding = '15px';
+// elem2.style.padding = '10px';
 
-const random_color = () =>{
-         const r = Math.round(Math.random() * 255);
-         const g = Math.round(Math.random() * 255);
-         const b = Math.round(Math.random() * 255);
-         return `rgb(${r},${g}, ${b})`
-     }
 
-root.addEventListener('click', () =>{
-              root.style.color = random_color();
-              root.style.backgroundColor = random_color();
-              root.style.padding = '20px';
+// const elem3 = document.querySelector('.root .price');
+
+// elem3.innerText = 'Price: 140€';
+
+// elem3.style.padding = '15px';
+
+// const random_color = () =>{
+//          const r = Math.round(Math.random() * 255);
+//          const g = Math.round(Math.random() * 255);
+//          const b = Math.round(Math.random() * 255);
+//          return `rgb(${r},${g}, ${b})`
+//      }
+
+// root.addEventListener('click', () =>{
+//               root.style.color = random_color();
+//               root.style.backgroundColor = random_color();
+//               root.style.padding = '20px';
              
-          })
+//           })
+
+
+const rootElem = document.querySelector('.root');
+
+const card = document.createElement('div');
+const art = document.createElement('p');
+const title = document.createElement('p');
+const price = document.createElement('p');
+
+art.innerText = 'Articul: 1234';
+title.innerText = 'Title: iPhone 13 Pro';
+price.innerText = 'Price: 1300';
+
+card.innerHTML = '<p>hello</p>';
+
+card.classList.add('card');
+
+card.append(art, title, price);
+
+rootElem.append(card);
+
+
 
 
 
