b81c88a859588dc3edf68f1bb227d551d6b5060a
mariam-85 <mariam.lofink85@gmail.com>
2022-10-17 10:25:50 +0200

    homework added

---
+++ Specialization_Frontend/HW_Fetch/scripts/script.js
@@ -0,0 +1,33 @@
+//const cards_container = document.querySelector('.cards_container');
+
+const tasks_container = document.querySelector('#tasks_container');
+
+
+fetch('https://jsonplaceholder.typicode.com/todos')
+.then(resp => resp.json())
+.then(json => render(json));
+
+const render = (json) => {
+    const tasks = json.map(({ title, completed }) => {
+        const task = document.createElement('div');
+
+        const titleElem = document.createElement('p');
+        const completedElem = document.createElement('p');
+
+        const status = completed ? 'done' : 'not done';
+
+        const background = completed ? 'lightgreen' : 'lightgray';
+
+        task.style.backgroundColor = background;
+
+        titleElem.innerText = `Task: ${title}`;
+        
+        completedElem.innerText = `Status: ${status}`;
+
+        task.classList.add('task_card');
+
+        task.append(titleElem, completedElem);
+        return task
+    })
+        tasks_container.append(...tasks)
+}
