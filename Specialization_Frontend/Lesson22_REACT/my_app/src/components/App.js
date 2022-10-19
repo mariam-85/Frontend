1a86b2706529e975845ba86fabc1082a48b4dbea
mariam-85 <mariam.lofink85@gmail.com>
2022-10-19 12:42:35 +0200

    first REACT App

---
+++ Specialization_Frontend/Lesson22_REACT/my_app/src/components/App.js
@@ -0,0 +1,16 @@
+import Example from "./Example";
+
+function App() {
+  return (
+    <div>
+      <Example first_name='Oleg'
+      last_name='Petrov'/>
+      <Example first_name='Anna'
+      last_name='Ivanova'/>
+      <Example first_name='Irina'
+      last_name='Sidorova'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Specialization_Frontend/Lesson22_REACT/my_app/src/components/Example.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Example({ first_name, last_name }) {
+  return (
+    <div>
+       <p>First name: {first_name}</p>
+       <p>Last name: {last_name}</p>
+
+    </div>
+  )
+}
