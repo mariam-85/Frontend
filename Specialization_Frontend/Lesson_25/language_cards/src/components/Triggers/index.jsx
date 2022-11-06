b90add07a74371eb48621a4fe51da9bdf3784415
mariam-85 <mariam.lofink85@gmail.com>
2022-11-07 00:30:05 +0100

    changes added

--- Specialization_Frontend/Lesson_25/language_cards/src/components/Triggers/index.jsx
+++ Specialization_Frontend/Lesson_25/language_cards/src/components/Triggers/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import style from './index.module.css'
 
 export default function Triggers({ change_to_eng, change_to_rus }) {
   return (
-    <div className={StyleSheet.triggers}>
+    <div className={style.triggers}>
         <button onClick={change_to_eng}>Eng</button>
         <button onClick={change_to_rus}>Rus</button>
     </div>
