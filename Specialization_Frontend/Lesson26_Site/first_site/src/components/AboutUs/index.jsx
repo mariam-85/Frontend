dba36ed52287eda3c3f5f3b042af5f910cb9458f
mariam-85 <mariam.lofink85@gmail.com>
2022-11-10 22:50:39 +0100

    style changes added

--- Specialization_Frontend/Lesson26_Site/first_site/src/components/AboutUs/index.jsx
+++ Specialization_Frontend/Lesson26_Site/first_site/src/components/AboutUs/index.jsx
@@ -8,6 +8,7 @@ export default function AboutUs() {
 <section className={style.about_section}>
     <div className={['wrapper', style.about_block].join(' ')}>
         <p className='subheader'>О нас</p>
+        <p className='text'>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн.</p>
         <h2>Компания ИвановПром</h2>
 
         <div className={style.about_container}>
