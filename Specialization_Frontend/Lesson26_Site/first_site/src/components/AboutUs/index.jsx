055d966ac842c9097e5488e73d01a11fa9daca6a
mariam-85 <mariam.lofink85@gmail.com>
2022-11-11 11:00:59 +0100

    about_us section added

--- Specialization_Frontend/Lesson26_Site/first_site/src/components/AboutUs/index.jsx
+++ Specialization_Frontend/Lesson26_Site/first_site/src/components/AboutUs/index.jsx
@@ -5,19 +5,23 @@ import style from './index.module.css'
 
 export default function AboutUs() {
   return (
-<section className={style.about_section}>
-    <div className={['wrapper', style.about_block].join(' ')}>
-        <p className='subheader'>О нас</p>
-        <p className='text'>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн.</p>
-        <h2>Компания ИвановПром</h2>
+<section className={['wrapper', style.about_us_section].join(' ')}>
 
-        <div className={style.about_container}>
+    <div>
+        <p className='subheader'>О нас</p>
+        <h2>Компания <span className={style.accent}>ИвановПром</span></h2>
+        <p className={style.about_us_info}>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн.</p>
+
+    </div>
+        
+
+        <div className={style.about_us_cards}>
             {
                 aboutUs.map(el => <AboutUsCard key={el.id} {...el} />)
 
             }
         </div>
-    </div>
+    
 </section>
 
   )
--- Specialization_Frontend/Lesson26_Site/first_site/src/components/AboutUsCard/index.jsx
+++ Specialization_Frontend/Lesson26_Site/first_site/src/components/AboutUsCard/index.jsx
@@ -3,19 +3,13 @@ import style from './index.module.css'
 
 export default function AboutUsCard({ value, title, link_title, link_url } ) {
 
-    const content = {}
     
-    const about_style = {
-        backgroundImage: 'grey'
-
-    }
   return (
 
-    <div style={about_style} className={style.about_card}>
+    <div className={style.about_us_card}>
         <p>{ value }</p> 
         <p>{ title }</p> 
-        <p>{ link_title }</p>
-        <p>{ link_url }</p> 
+        <a href={ link_url }>{ link_title }</a> 
 
 
     </div>
