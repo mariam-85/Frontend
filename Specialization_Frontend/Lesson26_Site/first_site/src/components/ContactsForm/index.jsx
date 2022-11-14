ca16eb87a2b6135ad20973f8d47aa1fc132997fa
mariam-85 <mariam.lofink85@gmail.com>
2022-11-14 10:22:28 +0100

    contacts_form added

--- Specialization_Frontend/Lesson26_Site/first_site/src/components/ContactsForm/index.jsx
+++ Specialization_Frontend/Lesson26_Site/first_site/src/components/ContactsForm/index.jsx
@@ -3,6 +3,27 @@ import style from './index.module.css'
 
 export default function ContactsForm() {
   return (
-    <div className={style.contacts_form}>ContactsForm</div>
-  )
-}
+    <form className={style.contacts_form}>
+
+            <div className={style.firstname_lastname}>
+        <label>
+            <p>Name</p>
+            <input type="text"  name='firstname' placeholder='Ivan'/>
+        </label>
+
+        <label>
+            <p>Family name</p>
+            <input type="text"  name='lastname' placeholder='Ivanov'/>
+        </label>
+        </div>
+
+        <label>
+            <p>Message</p>
+            <textarea type="text"  name='message' placeholder='Your message'/>
+
+        </label>
+
+        <button>Send message</button>
+    </form>
+    )
+    }
