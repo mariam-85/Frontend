77d9a8e1af2d150da1620b94881a2f057fb86e0b
mariam-85 <mariam.lofink85@gmail.com>
2022-11-16 23:25:29 +0100

    menu adaptive added

--- Specialization_Frontend/Lesson26_Site/first_site/src/components/App/index.jsx
+++ Specialization_Frontend/Lesson26_Site/first_site/src/components/App/index.jsx
@@ -4,11 +4,15 @@ import Services from '../Services'
 import AboutUs from '../AboutUs'
 import Contacts from '../Contacts'
 import Footer from '../Footer'
+import { useState } from 'react'
 
 export default function App() {
+
+const[ menuActive, setMenuActive ] = useState (true);
+
   return (
     <div>
-      <Nav />
+      <Nav menuActive={menuActive} setMenuActive={setMenuActive}/>
       <Services />
       <AboutUs />
       <Contacts />
--- Specialization_Frontend/Lesson26_Site/first_site/src/components/Nav/index.jsx
+++ Specialization_Frontend/Lesson26_Site/first_site/src/components/Nav/index.jsx
@@ -3,13 +3,15 @@ import logo from './media/Group 1.png'
 import style from './index.module.css'
 import { MenuOutlined } from '@ant-design/icons'
 
-export default function Nav() {
+export default function Nav( { menuActive, setMenuActive }) {
+
+  console.log(menuActive);
   return (
     <nav className={['wrapper', style.nav_block].join(' ')}>
         <a href='/#'>
             <img src={logo} alt="logo" />
         </a>
-        <ul className={style.nav_menu}>
+        <ul className={[ style.nav_menu, menuActive ? style.active : '' ].join(' ')}>
             <li>Главная</li>
             <li>Услуги</li>
             <li>Kейсы</li>
@@ -17,7 +19,7 @@ export default function Nav() {
             <li>Kонтакты</li>
         </ul>
 
-        <MenuOutlined className={style.menu_icon}/>
+        <MenuOutlined className={style.menu_icon} onClick={() => setMenuActive(menuActive ? false : true)}/>
 
 
     </nav>
