3214f85b33589b8d0238ee295b2114345dd576fc
mariam-85 <mariam.lofink85@gmail.com>
2022-11-17 23:17:46 +0100

    user_form added

--- Specialization_Frontend/Teams_master/teams_app/src/components/AddUserForm/index.jsx
+++ Specialization_Frontend/Teams_master/teams_app/src/components/AddUserForm/index.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import style from './index.module.css'
 
 export default function AddUserForm() {
-  return (
-    <div>AddUserForm</div>
-  )
-}
+
+    const submit_form = event => {
+        event.preventDefault();
+        const { user_title } = event.target;
+        console.log(user_title.value);
+        user_title.value = '';
+    }
+    
+      return (
+        <form className={style.add_user_form} onSubmit={submit_form}>
+            <p>Add user</p>
+            <input type="text" name='user_title' placeholder='User´s title' />
+            <button>Add user</button>
+        </form>
+      )
+    }
+
