5b3889e621d55364d3554b8c09ca3d83c633601b
mariam-85 <mariam.lofink85@gmail.com>
2022-11-24 23:11:23 +0100

    add_post added

---
+++ Specialization_Frontend/Post_Master/post_master/src/components/App/index.jsx
@@ -0,0 +1,40 @@
+import PostsContainer from "../PostsContainer";
+import { useState } from 'react'
+import { posts_data } from '../../data/posts'
+import { Context } from '../../context'
+import AddPostForm from "../AddPostForm";
+
+export default function App() {
+
+  const [ posts, setPosts ] = useState(posts_data);
+
+  const change_like = (id) => {
+    // Найти карточку по айди - метод find()
+    const target_card = posts.find(el => el.id === id);
+
+    // У найденной карточки меняем значение свойства like на противоположное
+    target_card.like = !target_card.like;
+
+    // Отследить изменение состояния (setPosts)
+    setPosts([...posts]);
+  }
+
+  const add_post = (title_value, text_value) => {
+    setPosts([...posts, {
+      id: Date.now(),
+      title: title_value,
+      text: text_value
+    }])
+  }
+
+  return (
+    <div>
+      <Context.Provider value={{ posts, change_like, add_post }}>
+        <AddPostForm />
+        <PostsContainer />
+      </Context.Provider>
+    </div>
+  );
+}
+
+
---
+++ Specialization_Frontend/Post_Master/post_master/src/components/Post/index.jsx
@@ -0,0 +1,30 @@
+import React, { useContext} from 'react'
+import style from './index.module.css'
+import { Context } from '../../context';
+
+
+export default function Post({ id, title, text, like }) {
+
+  const { change_like } = useContext(Context);
+
+  const like_or_not = like ? 'Liked' : 'Like?';
+
+  const like_style = [style.like_btn, like ? style.like_btn_active : ''].join(' ')
+
+
+
+  return (
+
+    <div className={style.post_item}>
+
+      <p>{ title }</p>
+      <p>{ text }</p>
+      <p 
+      className={like_style} 
+      onClick={() => change_like(id)}
+      >
+        { like_or_not }</p>
+
+    </div>
+  )
+}
