12e231e16502777e1a510e6633841e923c683cfb
mariam-85 <mariam.lofink85@gmail.com>
2022-11-25 12:43:37 +0100

    local storage added

--- Specialization_Frontend/Lesson_25/language_cards/src/components/AddPostForm/index.jsx
+++ Specialization_Frontend/Lesson_25/language_cards/src/components/AddPostForm/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import style from './index.module.css'
 
-export default function AddPostForm() {
+export default function AddPostForm({ add_card }) {
 
-    const form_submit = () => {
-
+    const form_submit = (event) => {
+        event.preventDefault();
+        const { rus, eng } = event.target;
+        add_card(rus.value, eng.value);
+        rus.value = '';
+        eng.value = '';
     }
+
   return (
     <div>
-        <form onSubmit={form_submit}>
-            
+        <form onSubmit={form_submit} className={style.add_form}>
+            <input type="text" name='rus' placeholder='Russian' />
+            <input type="text" name='eng' placeholder='English' />
+            <button>Add</button>
         </form>
     </div>
   )
--- Specialization_Frontend/Lesson_25/language_cards/src/components/App/index.jsx
+++ Specialization_Frontend/Lesson_25/language_cards/src/components/App/index.jsx
@@ -1,12 +1,24 @@
 import CardsContainer from "../CardsContainer";
 import { words } from "../../data/words";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Triggers from '../Triggers';
 import AddPostForm from "../AddPostForm";
 
 export default function App() {
 
     const [ cards, setCards ] = useState(words);
+
+    useEffect(() => {
+        const res = JSON.parse(localStorage.getItem('cards'));
+        setCards(res)
+      }, []); // следить за состоянием не нужно, функция запускается один раз при перезагрузке страницы
+    
+    
+      useEffect(() => {
+        localStorage.setItem('cards', JSON.stringify(cards))
+      }, [cards]); // функция выпоняется каждый раз при изменении состояния cards
+    
+      // второй аргумент - указание на то, за каким состоянием надо следить
 
     const change_to_eng = () => {
         setCards(cards.map(el => {
@@ -31,6 +43,17 @@ const change_lang = (id) => {
     }))
 }
 
+const add_card = (rus_value, eng_value) => setCards([
+    ...cards,
+    {
+        id: Date.now(), // cards.length + 1
+        eng: eng_value,
+        rus: rus_value,
+        lang: 'eng'
+    }
+]);
+
+
 // const change_lang = (id) => {
   //   cards[id - 1].lang = cards[id - 1].lang === 'rus' ? 'eng' : 'rus';
   //   setCards([...cards]);
@@ -39,7 +62,7 @@ const change_lang = (id) => {
     return (
 
       <div>
-          <AddPostForm />
+          <AddPostForm add_card={add_card} />
           <CardsContainer words_array={cards} change_lang={change_lang} />
           <Triggers change_to_eng={change_to_eng} change_to_rus={change_to_rus} />
 
