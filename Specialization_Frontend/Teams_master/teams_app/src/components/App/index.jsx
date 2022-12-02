24e26cebe3a6c6c3cf98836653be5a76eeb2ce6d
mariam-85 <mariam.lofink85@gmail.com>
2022-12-02 12:44:26 +0100

    local storage added

--- Specialization_Frontend/Teams_master/teams_app/src/components/App/index.jsx
+++ Specialization_Frontend/Teams_master/teams_app/src/components/App/index.jsx
@@ -2,7 +2,7 @@ import ConfigurationsPage from '../../pages/ConfigurationsPage';
 import TeamsPage from '../../pages/TeamsPage';
 import Nav from '../Nav';
 import { Routes, Route } from 'react-router-dom'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Context } from '../../context'
 
 export default function App() {
@@ -12,6 +12,17 @@ const [ teams, setTeams ] = useState([]);
 
 const [ users, setUsers ] = useState([]);
 
+useEffect(() => {
+  const teams = JSON.parse(localStorage.getItem('teams'));
+  const users = JSON.parse(localStorage.getItem('users'));
+  if (teams) setTeams(teams);
+  if (users) setUsers(users);
+}, []);
+
+useEffect(() => {
+  localStorage.setItem('teams', JSON.stringify(teams));
+  localStorage.setItem('users', JSON.stringify(users));
+}, [teams, users]);
 const addTeam = (team_title) => {
   setTeams([...teams, { value: team_title, label: team_title}])
 }
